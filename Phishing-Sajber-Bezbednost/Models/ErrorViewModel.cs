b830fbd45cd56f4d12e68cd491f725e73a275813
DESKTOP-T87PFGJ\djord <djordjedjoricsoko@gmail.com>
2025-04-18 12:38:03 +0200

    Init

---
+++ Phishing-Sajber-Bezbednost/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Phishing_Sajber_Bezbednost.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
